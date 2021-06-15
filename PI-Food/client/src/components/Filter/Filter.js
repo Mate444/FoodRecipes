b888c25f691561f39d19b15588025ba4163bf3cb
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-06-15 20:12:42 -0300

    Update Filter.js

--- PI-Food/client/src/components/Filter/Filter.js
+++ PI-Food/client/src/components/Filter/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState, } from 'react';
+import React from 'react';
 import {useSelector} from 'react-redux';
 import style from './Filter.module.css'
 const Filter = ({ sortRecipes, handleClick, dispatch, filterRecipes, diets }) => {
